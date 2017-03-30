9ffca8f9553f86c504510dc5698ecd50910bf2ca
ipc103 <iancandysemail@gmail.com>
2017-03-30 10:45:33 -0400

    post lecture

--- lib/models/movie.rb
+++ lib/models/movie.rb
@@ -3,8 +3,26 @@ class Movie < ActiveRecord::Base
   has_many :movie_categories
   has_many :categories, through: :movie_categories
 
+  def self.average_rating
+    joins(:reviews).average(:rating).to_f
+  end
+
+  def average_rating_eighties_movies
+    self.eighties.average_rating
+  end
+
+  def self.eighties
+    # self.where('release_year BETWEEN 1980 AND 1989')
+    self.where({release_year: (1980..1989)})
+  end
+
+  # NOT PREFERRED
+  # def self.slow_eighties
+  #   Movie.all.select {|movie| movie.release_year.to_i <= 1989 && movie.release_year.to_i >= 1980}
+  # end
+
   def self.highest_rated
-    
+    joins(:reviews).group('movies.id').order('AVG(reviews.rating) DESC').first
   end
 
   def self.lowest_rated
@@ -16,7 +34,20 @@ class Movie < ActiveRecord::Base
   end
 
   def average_rating
-
+    self.reviews.average(:rating).to_f
   end
+
+  def number_of_reviews
+    reviews.count
+  end
+
+  # NOT PREFERRED 
+  # def average_rating
+  #   total_points = self.reviews.inject(0.0) do |total_points, review|
+  #     total_points + review.rating
+  #   end
+  #   total_points / self.reviews.length
+  #   # "SELECT AVG(reviews.rating) FROM reviews WHERE reviews.movie_id = ? GROUP BY reviews.movie_id"
+  # end
 
 end
